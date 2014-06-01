690fd67010ff9fd412d7dcbb8330f3895f91538f
fedemv@gmail.com <F12icernall>
2014-06-01 15:19:16 -0300

    se agregan funciones para convertir id_usuario de empresa y cliente se agrega funcion que te retorna el codigo de publicacion

--- DB/script_creacion_inicial.sql
+++ DB/script_creacion_inicial.sql
@@ -14,6 +14,27 @@ GO
 --contraseña default para todos (gdd)
 --5e4ac4f46b377c21b587cdaf94cc4e0d9bff2434dc00393dc4eef7b90f39ee01
 
+CREATE FUNCTION LOS_OPTIMISTAS.obtenerDNI(@dni numeric(18,0))
+RETURNS varchar(20)
+BEGIN
+	RETURN LTRIM(STR(CONVERT(INT,STR(@dni,20))))
+END
+GO
+
+CREATE FUNCTION LOS_OPTIMISTAS.obtenerCuit(@cuit varchar(50))
+RETURNS varchar(20)
+BEGIN
+	RETURN LTRIM(STR(CONVERT(INT,SubString(@cuit,4,8))))
+END
+GO
+
+CREATE FUNCTION LOS_OPTIMISTAS.obtenerCodigoArticulo(@descripcion varchar(255))
+returns Int
+BEGIN
+	RETURN CONVERT(INT,RTRIM(SubString(@descripcion,24,20)))
+END
+GO
+
 CREATE TABLE [LOS_OPTIMISTAS].[Usuario](
 	[Id_Usuario][varchar](20) NOT NULL,
 	[Password][varchar](64) NOT NULL,
@@ -30,11 +51,11 @@ CREATE TABLE[LOS_OPTIMISTAS].[Usuario_temp](
 
 --Devuelve dni
 INSERT INTO LOS_OPTIMISTAS.Usuario_temp(Id_Usuario,Rol)
-select Distinct(LTRIM(STR(CONVERT(INT,STR(Publ_Cli_Dni,20))))),2 from gd_esquema.Maestra WHERE Publ_Cli_Dni IS NOT NULL
+select Distinct(LOS_OPTIMISTAS.obtenerDNI(Publ_Cli_Dni)),2 from gd_esquema.Maestra WHERE Publ_Cli_Dni IS NOT NULL
 
 --Devuelve numero razon social
 INSERT INTO LOS_OPTIMISTAS.Usuario_temp(Id_Usuario,Rol)
-select Distinct(LTRIM(STR(CONVERT(INT,SubString(Publ_Empresa_Cuit,4,8))))),3 from gd_esquema.Maestra WHERE Publ_Empresa_Cuit IS NOT NULL
+select Distinct(LOS_OPTIMISTAS.obtenerCuit(Publ_Empresa_Cuit)),3 from gd_esquema.Maestra WHERE Publ_Empresa_Cuit IS NOT NULL
 
 --Meto toda la lista de la temporal en Usuario
 INSERT INTO LOS_OPTIMISTAS.Usuario(Id_Usuario,Password,Cantidad_Login,Ultima_Fecha)
@@ -110,8 +131,8 @@ CREATE TABLE [LOS_OPTIMISTAS].[Cliente](
 	[Id_Usuario][varchar](20) NOT NULL,
 	[Dni][varchar](20) NOT NULL,---El DNI y el Id_Usuario es lo mismo, los cargo igual.
 	[Tipo_Documento][varchar](4) NOT NULL,----No es siempre DNI en este caso?
-	[Nombre][varchar](20),
-	[Apellido][varchar](20),
+	[Nombre][varchar](40),
+	[Apellido][varchar](40),
 	[Fecha_Nacimiento][datetime]
 	
 		CONSTRAINT [FK_Cliente_Usuario_Id_Usuario] FOREIGN KEY(Id_Usuario)
@@ -123,14 +144,13 @@ CREATE TABLE [LOS_OPTIMISTAS].[Cliente](
 
 --CARGO LOS CLIENTES
 INSERT INTO LOS_OPTIMISTAS.Cliente(Id_Usuario,Dni,Tipo_Documento,Nombre,Apellido,Fecha_Nacimiento)
-select Distinct(LTRIM(STR(CONVERT(INT,STR(Publ_Cli_Dni,20))))),LTRIM(STR(CONVERT(INT,STR(Publ_Cli_Dni,20)))),'DNI',Publ_Cli_Nombre,Publ_Cli_Apeliido,Publ_Cli_Fecha_Nac from gd_esquema.Maestra WHERE Publ_Cli_Dni IS NOT NULL
+select Distinct(LOS_OPTIMISTAS.obtenerDNI(Publ_Cli_Dni)),LOS_OPTIMISTAS.obtenerDNI(Publ_Cli_Dni),'DNI',Publ_Cli_Nombre,Publ_Cli_Apeliido,Publ_Cli_Fecha_Nac from gd_esquema.Maestra WHERE Publ_Cli_Dni IS NOT NULL
 
 --TABLA EMPRESA
 CREATE TABLE [LOS_OPTIMISTAS].[Empresa](
-
-[Razon_social] [varchar](20) NOT NULL ,
+[ID_Usuario] [varchar](20) NOT NULL,
+[Razon_social] [varchar](255) NOT NULL ,
 [Cuit] [varchar](20) NOT NULL,
-[Id_Usuario] [varchar](20) NOT NULL,
 [Ciudad][varchar](20) NULL,
 [Nombre_Contacto][varchar](20) NULL, 
 [Fecha_Creacion][datetime]
@@ -143,18 +163,18 @@ CONSTRAINT [FK_Empresa_Cliente_Id_Usuario] FOREIGN KEY(Id_Usuario)
 		
 )
 --CARGO LAS EMPRESAS
-INSERT INTO LOS_OPTIMISTAS.Empresa(ID_Usuario,Cuit,Razon_social,Ciudad,Nombre_Contacto,Fecha_Creacion)
-select Distinct(LTRIM(STR(CONVERT(INT,SubString(Publ_Empresa_Cuit,4,8))))), Publ_Empresa_Cuit,Publ_Empresa_Razon_Social,null, null,Publ_Empresa_Fecha_Creacion from gd_esquema.Maestra WHERE Publ_Empresa_Cuit IS NOT NULL
+INSERT INTO LOS_OPTIMISTAS.Empresa(ID_Usuario,Razon_social,Cuit,Ciudad,Nombre_Contacto,Fecha_Creacion)
+select Distinct(LOS_OPTIMISTAS.obtenerCuit(Publ_Empresa_Cuit)),Publ_Empresa_Razon_Social,Publ_Empresa_Cuit,null, null,Publ_Empresa_Fecha_Creacion from gd_esquema.Maestra WHERE Publ_Empresa_Cuit IS NOT NULL
 
 --CREO TABLA DOM_MAIL
 
 CREATE TABLE [LOS_OPTIMISTAS].[Dom_Mail](
 
 [Id_Usuario][varchar](20) NOT NULL ,
-[Domicilio][varchar](40)  NULL,
-[Telefono][varchar](20)  NULL,
+[Domicilio][varchar](100)  NULL,
+[Telefono][varchar](255)  NULL,
 [Cp] [varchar](10)  NULL,
-[Mail] [varchar](40) NULL, 
+[Mail] [varchar](255) NULL, 
 [Localidad] [varchar](40) NULL
 
 CONSTRAINT [FK_Dom_Mail_Empresa_Id_Usuario] FOREIGN KEY(Id_Usuario)
@@ -164,9 +184,9 @@ CONSTRAINT [FK_Dom_Mail_Empresa_Id_Usuario] FOREIGN KEY(Id_Usuario)
 --CARGO TABLA Dom_Mail con los datos de Clientes
 
 INSERT INTO LOS_OPTIMISTAS.Dom_Mail(Id_Usuario,Domicilio,Telefono,Cp,Mail,Localidad)
-select Distinct(LTRIM(STR(CONVERT(INT,STR(Publ_Cli_Dni,20))))),Cli_Dom_Calle,null,Cli_Cod_Postal,Cli_Mail,null from gd_esquema.Maestra WHERE Publ_Cli_Dni IS NOT NULL
+select Distinct(LOS_OPTIMISTAS.obtenerDNI(Publ_Cli_Dni)),Cli_Dom_Calle,null,Cli_Cod_Postal,Cli_Mail,null from gd_esquema.Maestra WHERE Publ_Cli_Dni IS NOT NULL
 
 --CARGO TABLA Dom_Mail con los datos de las Empresas
 
 INSERT INTO LOS_OPTIMISTAS.Dom_Mail(Id_Usuario,Domicilio,Telefono,Cp,Mail,Localidad)
-select Distinct(LTRIM(STR(CONVERT(INT,SubString(Publ_Empresa_Cuit,4,8))))),Publ_Empresa_Dom_Calle,null,Publ_Empresa_Cod_Postal,Publ_Empresa_Mail,null from gd_esquema.Maestra WHERE Publ_Empresa_Cuit IS NOT NULL
+select Distinct(LOS_OPTIMISTAS.obtenerCuit(Publ_Empresa_Cuit)),Publ_Empresa_Dom_Calle,null,Publ_Empresa_Cod_Postal,Publ_Empresa_Mail,null from gd_esquema.Maestra WHERE Publ_Empresa_Cuit IS NOT NULL
