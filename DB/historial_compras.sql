b61f259a5b53cca54b09c5e05878274248282683
fedemv@gmail.com <F12icernall>
2014-06-27 04:25:48 -0300

    Se agrega historial compras al script de inicializacion

--- DB/historial_compras.sql
+++ DB/historial_compras.sql
@@ -18,10 +18,17 @@ REFERENCES [LOS_OPTIMISTAS].[Stock](Id_Articulo)
 )
 --COMPRAS POR CLIENTES A CLIENTES
 INSERT INTO LOS_OPTIMISTAS.Historial_Compra(Id_Vendedor, Id_Comprador, Id_Publicacion, Id_Articulo, Compra_Cantidad, Compra_Fecha)
-select LOS_OPTIMISTAS.obtenerDNI(Publ_Cli_Dni), LOS_OPTIMISTAS.obtenerDNI(Cli_Dni), Publicacion_Cod, LOS_OPTIMISTAS.obtenerCodigoArticulo(Publicacion_Descripcion), Compra_Cantidad, Compra_Fecha
-FROM gd_esquema.Maestra WHERE Cli_Dni IS NOT NULL AND Publ_Cli_Dni IS NOT NULL AND Publicacion_Tipo != 'Subasta'
+SELECT LOS_OPTIMISTAS.obtenerDNI(Publ_Cli_Dni), LOS_OPTIMISTAS.obtenerDNI(Cli_Dni), Publicacion_Cod, 
+	LOS_OPTIMISTAS.obtenerCodigoArticulo(Publicacion_Descripcion), Compra_Cantidad, Compra_Fecha
+	FROM gd_esquema.Maestra 
+	WHERE Cli_Dni IS NOT NULL AND Publ_Cli_Dni IS NOT NULL
+	AND Compra_Cantidad IS NOT NULL AND Compra_Fecha IS NOT NULL
+	AND Calificacion_Cant_Estrellas IS NULL
 
 --COMPRAS POR CLIENTES A EMPRESAS
 INSERT INTO LOS_OPTIMISTAS.Historial_Compra(Id_Vendedor, Id_Comprador, Id_Publicacion, Id_Articulo,Compra_Cantidad, Compra_Fecha)
-select LOS_OPTIMISTAS.obtenerCuit(Publ_Empresa_Cuit), LOS_OPTIMISTAS.obtenerDNI(Cli_Dni), Publicacion_Cod, LOS_OPTIMISTAS.obtenerCodigoArticulo(Publicacion_Descripcion), Compra_Cantidad, Compra_Fecha
-FROM gd_esquema.Maestra WHERE Cli_Dni IS NOT NULL AND Publ_Empresa_Cuit IS NOT NULL AND Publicacion_Tipo != 'Subasta'
+SELECT LOS_OPTIMISTAS.obtenerCuit(Publ_Empresa_Cuit), LOS_OPTIMISTAS.obtenerDNI(Cli_Dni), 
+	Publicacion_Cod, LOS_OPTIMISTAS.obtenerCodigoArticulo(Publicacion_Descripcion), Compra_Cantidad, Compra_Fecha
+	FROM gd_esquema.Maestra WHERE Cli_Dni IS NOT NULL AND Publ_Empresa_Cuit IS NOT NULL
+	AND Compra_Cantidad IS NOT NULL AND Compra_Fecha IS NOT NULL
+	AND Calificacion_Cant_Estrellas IS NULL
--- DB/script_creacion_inicial.sql
+++ DB/script_creacion_inicial.sql
@@ -541,6 +541,41 @@ GO
 ALTER TABLE [LOS_OPTIMISTAS].[Publicacion] ADD CONSTRAINT [FK_Publicacion_Id_Articulo] FOREIGN KEY (Id_Articulo)
 	REFERENCES [LOS_OPTIMISTAS].[Stock](Id_Articulo)
 
+--CREO TABLA HISTORIAL DE COMPRAS
+CREATE TABLE [LOS_OPTIMISTAS].[Historial_Compra](
+[Id_Vendedor][varchar](20) NOT NULL,
+[Id_Comprador][varchar](20) NOT NULL,
+[Id_Publicacion][numeric](18,0) NOT NULL,
+[Id_Articulo][numeric](18,0) NOT NULL,
+[Compra_Cantidad][numeric](18,0) NOT NULL,
+[Compra_Fecha][smalldatetime] NOT NULL,
+
+CONSTRAINT [FK_Historial_Compra_Id_Vendedor] FOREIGN KEY(Id_Vendedor)
+REFERENCES [LOS_OPTIMISTAS].[Usuario](Id_Usuario),
+CONSTRAINT [FK_Historial_Compra_Id_Comprador] FOREIGN KEY(Id_Comprador)
+REFERENCES [LOS_OPTIMISTAS].[Usuario](Id_Usuario),
+CONSTRAINT [FK_Historial_Compra_Id_Publicacion] FOREIGN KEY(Id_Publicacion)
+REFERENCES [LOS_OPTIMISTAS].[Publicacion](Id_Publicacion),
+CONSTRAINT [FK_Historial_Compra_Id_Articulo] FOREIGN KEY(Id_Articulo)
+REFERENCES [LOS_OPTIMISTAS].[Stock](Id_Articulo)
+)
+--HISTORIAL COMPRAS POR CLIENTES A CLIENTES
+INSERT INTO LOS_OPTIMISTAS.Historial_Compra(Id_Vendedor, Id_Comprador, Id_Publicacion, Id_Articulo, Compra_Cantidad, Compra_Fecha)
+SELECT LOS_OPTIMISTAS.obtenerDNI(Publ_Cli_Dni), LOS_OPTIMISTAS.obtenerDNI(Cli_Dni), Publicacion_Cod, 
+	LOS_OPTIMISTAS.obtenerCodigoArticulo(Publicacion_Descripcion), Compra_Cantidad, Compra_Fecha
+	FROM gd_esquema.Maestra 
+	WHERE Cli_Dni IS NOT NULL AND Publ_Cli_Dni IS NOT NULL
+	AND Compra_Cantidad IS NOT NULL AND Compra_Fecha IS NOT NULL
+	AND Calificacion_Cant_Estrellas IS NULL
+
+--HISTORIAL COMPRAS POR CLIENTES A EMPRESAS
+INSERT INTO LOS_OPTIMISTAS.Historial_Compra(Id_Vendedor, Id_Comprador, Id_Publicacion, Id_Articulo,Compra_Cantidad, Compra_Fecha)
+SELECT LOS_OPTIMISTAS.obtenerCuit(Publ_Empresa_Cuit), LOS_OPTIMISTAS.obtenerDNI(Cli_Dni), 
+	Publicacion_Cod, LOS_OPTIMISTAS.obtenerCodigoArticulo(Publicacion_Descripcion), Compra_Cantidad, Compra_Fecha
+	FROM gd_esquema.Maestra WHERE Cli_Dni IS NOT NULL AND Publ_Empresa_Cuit IS NOT NULL
+	AND Compra_Cantidad IS NOT NULL AND Compra_Fecha IS NOT NULL
+	AND Calificacion_Cant_Estrellas IS NULL
+
 --CREO TABLA FACTURACION DETALLE
 CREATE TABLE [LOS_OPTIMISTAS].[Facturacion_Detalle](
 	[Id_Factura][numeric](18,0) NOT NULL,
