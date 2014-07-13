2c0a336d388064c934d6bcde669692e1e7d6b620
fedemv@gmail.com <F12icernall>
2014-07-13 05:18:47 -0300

    Se agrega abm visibilidad

---
+++ FrbaCommerce/Abm Visibilidad/FormABMVisibilidad.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaCommerce.Abm_Visibilidad
+{
+    public partial class FormABMVisibilidad : Form
+    {
+        public FormABMVisibilidad()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormABMVisibilidadModificar formABMVisibilidadModificar = new FormABMVisibilidadModificar();
+            formABMVisibilidadModificar.MdiParent = this.MdiParent;
+            MdiParent.Size = formABMVisibilidadModificar.Size + Constantes.aumentoTamanio;
+            formABMVisibilidadModificar.Show();
+            this.Close();
+        }
+
+        private void FormABMVisibilidad_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dgvVisibilidad.CurrentRow != null)
+            {
+                Visibilidad visibilidad = new Visibilidad();
+                visibilidad.id = Convert.ToInt32(dgvVisibilidad.CurrentRow.Cells[0].Value);
+                visibilidad.description = Convert.ToString(dgvVisibilidad.CurrentRow.Cells[1].Value);
+                visibilidad.price = Convert.ToDouble(dgvVisibilidad.CurrentRow.Cells[2].Value);
+                visibilidad.percentage = Convert.ToDouble(dgvVisibilidad.CurrentRow.Cells[3].Value);
+                visibilidad.weight = Convert.ToInt32(dgvVisibilidad.CurrentRow.Cells[4].Value);
+                visibilidad.enable = Convert.ToInt32(dgvVisibilidad.CurrentRow.Cells[5].Value);
+                FormABMVisibilidadModificar formABMVisibilidadModificar = new FormABMVisibilidadModificar(visibilidad);
+                formABMVisibilidadModificar.MdiParent = this.MdiParent;
+                MdiParent.Size = formABMVisibilidadModificar.Size + Constantes.aumentoTamanio;
+                formABMVisibilidadModificar.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar un registro de la tabla a modificar");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dgvVisibilidad.CurrentRow != null)
+            {
+                Visibilidad visibilidad = new Visibilidad();
+                Visibilidad.deshabilitarVisibilidad(Convert.ToString(dgvVisibilidad.CurrentRow.Cells[0].Value));
+                btnClean.PerformClick();
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar un registro de la tabla a modificar");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Visibilidad.buscar(textCode.Text, textDescription.Text, textWeight.Text, dgvVisibilidad);
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            Procedimientos.limpiarTextBoxes(this);
+            Procedimientos.limpiarDataGridViews(dgvVisibilidad);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ FrbaCommerce/Abm Visibilidad/FormABMVisibilidadModificar.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace FrbaCommerce.Abm_Visibilidad
+{
+    partial class FormABMVisibilidadModificar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkEnable = new System.Windows.Forms.CheckBox();
+            this.textWeight = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.textPercentage = new System.Windows.Forms.TextBox();
+            this.textPrice = new System.Windows.Forms.TextBox();
+            this.textDescription = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // chkEnable
+            // 
+            this.chkEnable.AutoSize = true;
+            this.chkEnable.Location = new System.Drawing.Point(102, 233);
+            this.chkEnable.Name = "chkEnable";
+            this.chkEnable.Size = new System.Drawing.Size(73, 17);
+            this.chkEnable.TabIndex = 26;
+            this.chkEnable.Text = "Habilitado";
+            this.chkEnable.UseVisualStyleBackColor = true;
+            // 
+            // textWeight
+            // 
+            this.textWeight.Location = new System.Drawing.Point(137, 189);
+            this.textWeight.Name = "textWeight";
+            this.textWeight.Size = new System.Drawing.Size(100, 20);
+            this.textWeight.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Peso";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(150, 268);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 33);
+            this.btnSave.TabIndex = 23;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(44, 268);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(88, 33);
+            this.btnBack.TabIndex = 22;
+            this.btnBack.Text = "Cancelar";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // textPercentage
+            // 
+            this.textPercentage.Location = new System.Drawing.Point(137, 147);
+            this.textPercentage.Name = "textPercentage";
+            this.textPercentage.Size = new System.Drawing.Size(100, 20);
+            this.textPercentage.TabIndex = 21;
+            // 
+            // textPrice
+            // 
+            this.textPrice.Location = new System.Drawing.Point(137, 99);
+            this.textPrice.Name = "textPrice";
+            this.textPrice.Size = new System.Drawing.Size(100, 20);
+            this.textPrice.TabIndex = 20;
+            // 
+            // textDescription
+            // 
+            this.textDescription.Location = new System.Drawing.Point(137, 52);
+            this.textDescription.Name = "textDescription";
+            this.textDescription.Size = new System.Drawing.Size(100, 20);
+            this.textDescription.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Porcentaje";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Precio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Descripcion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Codigo";
+            // 
+            // textCode
+            // 
+            this.textCode.Location = new System.Drawing.Point(137, 12);
+            this.textCode.Name = "textCode";
+            this.textCode.Size = new System.Drawing.Size(100, 20);
+            this.textCode.TabIndex = 14;
+            // 
+            // FormABMVisibilidadModificar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 343);
+            this.Controls.Add(this.chkEnable);
+            this.Controls.Add(this.textWeight);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.textPercentage);
+            this.Controls.Add(this.textPrice);
+            this.Controls.Add(this.textDescription);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textCode);
+            this.Name = "FormABMVisibilidadModificar";
+            this.Text = "FormABMVisibilidadModificar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkEnable;
+        private System.Windows.Forms.TextBox textWeight;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.TextBox textPercentage;
+        private System.Windows.Forms.TextBox textPrice;
+        private System.Windows.Forms.TextBox textDescription;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textCode;
+    }
+}
