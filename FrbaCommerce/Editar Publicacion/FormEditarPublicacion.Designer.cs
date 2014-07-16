291c7b12d3f61acd674eaf31208f71cd09ba53d7
fedemv@gmail.com <F12icernall>
2014-07-15 22:21:28 -0300

    Agrego clases que el git no me tomo

---
+++ FrbaCommerce/Editar Publicacion/FormEditarPublicacion.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace FrbaCommerce.Editar_Publicacion
+{
+    partial class FormEditarPublicacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPublication = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbVisibility = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPublication)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPublication
+            // 
+            this.dgvPublication.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPublication.Location = new System.Drawing.Point(12, 127);
+            this.dgvPublication.Name = "dgvPublication";
+            this.dgvPublication.Size = new System.Drawing.Size(716, 150);
+            this.dgvPublication.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Descripcion";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(607, 90);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(120, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(15, 89);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(121, 23);
+            this.btnClean.TabIndex = 3;
+            this.btnClean.Text = "Limpiar";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(102, 10);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(155, 20);
+            this.txtDescription.TabIndex = 4;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(585, 9);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(142, 21);
+            this.cmbStatus.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(516, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Estado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(516, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Visibilidad";
+            // 
+            // cmbVisibility
+            // 
+            this.cmbVisibility.FormattingEnabled = true;
+            this.cmbVisibility.Location = new System.Drawing.Point(585, 48);
+            this.cmbVisibility.Name = "cmbVisibility";
+            this.cmbVisibility.Size = new System.Drawing.Size(142, 21);
+            this.cmbVisibility.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tipo";
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(102, 48);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(155, 21);
+            this.cmbType.TabIndex = 10;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(102, 288);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(155, 23);
+            this.btnBack.TabIndex = 11;
+            this.btnBack.Text = "Volver";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(535, 288);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(147, 23);
+            this.btnEdit.TabIndex = 12;
+            this.btnEdit.Text = "Editar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // FormEditarPublicacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 323);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbVisibility);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvPublication);
+            this.Name = "FormEditarPublicacion";
+            this.Text = "Editar Publicacion";
+            this.Load += new System.EventHandler(this.FormEditarPublicacion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPublication)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPublication;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbVisibility;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
---
+++ FrbaCommerce/Editar Publicacion/FormEditarPublicacion.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using FrbaCommerce.Generar_Publicacion;
+
+namespace FrbaCommerce.Editar_Publicacion
+{
+    public partial class FormEditarPublicacion : Form
+    {
+        public FormEditarPublicacion()
+        {
+            InitializeComponent();
+            this.cargarCombos();
+        }
+
+        private void FormEditarPublicacion_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            Procedimientos.limpiarTextBoxes(this);
+            Procedimientos.limpiarComboBoxes(this);
+            Procedimientos.limpiarDataGridViews(dgvPublication);
+        }
+
+        private void cargarCombos()
+        {
+            Procedimientos.LlenarComboBox(cmbStatus, "LOS_OPTIMISTAS.Estado", "Id_Estado", "Descripcion", null, null);
+            Procedimientos.LlenarComboBox(cmbVisibility, "LOS_OPTIMISTAS.Visibilidad", "Id_Visibilidad", "Descripcion", null, null);
+            Procedimientos.LlenarComboBox(cmbType, "LOS_OPTIMISTAS.Tipo_Publicacion", "Id_Tipo_Publicacion", "Descripcion", null, null);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = Constantes.procedimientoMostrarPublicaciones;
+
+            Usuarios usuario = VarGlobables.usuario;
+
+            command.Parameters.AddWithValue("@p_IdUsuario", usuario.user_id);
+
+            if (txtDescription.Text == string.Empty)
+                command.Parameters.AddWithValue("@p_Description", null);
+            else
+                command.Parameters.AddWithValue("@p_Description", txtDescription.Text);
+
+            if (cmbType.SelectedValue == null)
+                command.Parameters.AddWithValue("@p_Type", null);
+            else
+                command.Parameters.AddWithValue("@p_Type", cmbType.SelectedValue);
+
+            if (cmbStatus.SelectedValue == null)
+                command.Parameters.AddWithValue("@p_Status", null);
+            else
+                command.Parameters.AddWithValue("@p_Status", cmbStatus.SelectedValue);
+
+            if (cmbVisibility.SelectedValue == null)
+                command.Parameters.AddWithValue("@p_Visibility", null);
+            else
+                command.Parameters.AddWithValue("@p_Visibility", cmbVisibility.SelectedValue);
+
+            Procedimientos.llenarDataGridView(command, dgvPublication, "DataGridView Publicaciones");
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvPublication.CurrentRow == null)
+            {
+                MessageBox.Show("Debe seleccionar una publicacion a editar", "Frba Commerce", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string type = Convert.ToString(dgvPublication.CurrentRow.Cells[4].Value);
+            bool bidding = type.Equals("SUBASTA");
+            SqlConnection conn = Procedimientos.abrirConexion();
+            String nombreStoredProcedure = "LOS_OPTIMISTAS.proc_obtenerPublicacion";
+            SqlCommand command = new SqlCommand(nombreStoredProcedure, conn);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@p_Id_Publicacion", dgvPublication.CurrentRow.Cells[0].Value);
+            command.Parameters.AddWithValue("@p_Id_Usuario", VarGlobables.usuario.user_id);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            Publicacion publication = new Publicacion();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    publication.id = Convert.ToInt32(reader["Id_Publicacion"]);
+                    publication.description = reader["Descripcion"].ToString();
+                    publication.stock = Convert.ToInt32(reader["Stock"]);
+                    publication.status = Convert.ToInt32(reader["Id_Estado"]);
+                    publication.statusDescription = reader["estadoDescripcion"].ToString();
+                    publication.type = Convert.ToInt32(reader["Id_Tipo_Publicacion"]);
+                    publication.typeDescription = reader["tipoDescripcion"].ToString();
+                    publication.visibility = Convert.ToInt32(reader["Id_Visibilidad"]);
+                    publication.visibilityDescription = reader["visibilidadDescripcion"].ToString();
+                    publication.dateFrom = Convert.ToDateTime(reader["Fecha_Inicio"]);
+                    publication.dateTo = Convert.ToDateTime(reader["Fecha_Vencimiento"]);
+                    publication.prices = Convert.ToDouble(reader["Precio"]);
+                    publication.countForSale = Convert.ToInt32(reader["Cant_por_venta"]);
+                    publication.acceptQuestions = Convert.ToBoolean(reader["Permite_Preguntas"]);
+                }
+            }
+
+            Procedimientos.cerrarConexion(conn);
+            FormGenerarPublicacionAM formGenerarPublicacionAM = new FormGenerarPublicacionAM(bidding, publication);
+            formGenerarPublicacionAM.MdiParent = this.MdiParent;
+            MdiParent.Size = formGenerarPublicacionAM.Size + Constantes.aumentoTamanio;
+            formGenerarPublicacionAM.Show();
+            this.Close();
+        }
+    }
+}
